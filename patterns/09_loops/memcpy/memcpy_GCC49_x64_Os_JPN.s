d35330c6306c9f05e67629f57ee7f7adc92fc266
shmz <sysimizu@gmail.com>
2018-10-19 21:35:52 +0900

    Japanese: LOOPS

---
+++ patterns/09_loops/memcpy/memcpy_GCC49_x64_Os_JPN.s
@@ -0,0 +1,19 @@
+﻿my_memcpy:
+; RDI = コピー先アドレス
+; RSI = コピー元アドレス
+; RDX = ブロックのサイズ
+
+; カウンタ(i)を0で初期化
+	xor	eax, eax
+.L2:
+; 全バイトをコピーしたら終了する
+	cmp	rax, rdx
+	je	.L5
+; RSI+iのバイトをロードする:
+	mov	cl, BYTE PTR [rsi+rax]
+; バイトをRDI+iに保存する
+	mov	BYTE PTR [rdi+rax], cl
+	inc	rax ; i++
+	jmp	.L2
+.L5:
+	ret
---
+++ patterns/09_loops/simple/2_GCC_JPN.asm
@@ -0,0 +1,29 @@
+﻿                public main
+main            proc near
+
+var_20          = dword ptr -20h
+
+                push    ebp
+                mov     ebp, esp
+                and     esp, 0FFFFFFF0h
+                push    ebx
+                mov     ebx, 2    ; i=2
+                sub     esp, 1Ch
+
+; ラベルloc_80484D0（ループボディの開始場所）を16バイト境界でアラインメント
+                nop
+
+loc_80484D0:
+; (i)を第一引数としてprinting_function()に渡す
+                mov     [esp+20h+var_20], ebx 
+                add     ebx, 1    ; i++
+                call    printing_function
+                cmp     ebx, 64h  ; i==100か?
+                jnz     short loc_80484D0 ; そうでなければ継続
+                add     esp, 1Ch
+                xor     eax, eax  ; 0をリターン
+                pop     ebx
+                mov     esp, ebp
+                pop     ebp
+                retn
+main            endp
