72f42b4efa779c0b80750156ef805e310c08942d
shmz <sysimizu@gmail.com>
2018-10-22 21:49:25 +0900

    Japanese: MORE ABOUT STRINGS

--- patterns/10_strings/1_strlen/10_2_JPN.asm
+++ patterns/10_strings/1_strlen/10_2_JPN.asm
@@ -1,13 +1,13 @@
-_str$ = 8			; size = 4
+﻿_str$ = 8			; size = 4
 _strlen PROC
-	mov	edx, DWORD PTR _str$[esp-4] ; EDX -> pointer to the string
-	mov	eax, edx                    ; move to EAX
+	mov	edx, DWORD PTR _str$[esp-4] ; 文字列へのポインタをEDXに置く
+	mov	eax, edx                    ; EAXにコピー
 $LL2@strlen:
 	mov	cl, BYTE PTR [eax]          ; CL = *EAX
 	inc	eax                         ; EAX++
-	test	cl, cl                      ; CL==0?
-	jne	SHORT $LL2@strlen           ; no, continue loop
-	sub	eax, edx                    ; calculate pointers difference
-	dec	eax                         ; decrement EAX
+	test	cl, cl                      ; CL==0か
+	jne	SHORT $LL2@strlen           ; そうでなければ、ループを継続する
+	sub	eax, edx                    ; ポインタの差分を計算する
+	dec	eax                         ; EAXをデクリメントする
 	ret	0
 _strlen ENDP
