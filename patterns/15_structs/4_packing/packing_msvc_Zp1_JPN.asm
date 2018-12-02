026fe21233f471078cb2eadbb2bd7967dee07d3e
shmz <sysimizu@gmail.com>
2018-12-02 09:15:35 +0900

    Japanese: add 1.24 Structures

---
+++ patterns/15_structs/4_packing/packing_msvc_Zp1_JPN.asm
@@ -0,0 +1,43 @@
+﻿_main    PROC
+    push   ebp
+    mov    ebp, esp
+    sub    esp, 12
+    mov    BYTE PTR _tmp$[ebp], 1     ; フィールドaを設定
+    mov    DWORD PTR _tmp$[ebp+1], 2  ; フィールドbを設定
+    mov    BYTE PTR _tmp$[ebp+5], 3   ; フィールドcを設定
+    mov    DWORD PTR _tmp$[ebp+6], 4  ; フィールドdを設定
+    sub    esp, 12                    ; 一時的な構造体のために場所を確保
+    mov    eax, esp
+    mov    ecx, DWORD PTR _tmp$[ebp]  ; 10バイトコピー
+    mov    DWORD PTR [eax], ecx
+    mov    edx, DWORD PTR _tmp$[ebp+4]
+    mov    DWORD PTR [eax+4], edx
+    mov    cx, WORD PTR _tmp$[ebp+8]
+    mov    WORD PTR [eax+8], cx
+    call    _f
+    add    esp, 12
+    xor    eax, eax
+    mov    esp, ebp
+    pop    ebp
+    ret    0
+_main    ENDP
+
+_TEXT    SEGMENT
+_s$ = 8 ; size = 10
+?f@@YAXUs@@@Z PROC    ; f
+    push   ebp
+    mov    ebp, esp
+    mov    eax, DWORD PTR _s$[ebp+6]
+    push   eax
+    movsx  ecx, BYTE PTR _s$[ebp+5]
+    push   ecx
+    mov    edx, DWORD PTR _s$[ebp+1]
+    push   edx
+    movsx  eax, BYTE PTR _s$[ebp]
+    push   eax
+    push   OFFSET $SG3842
+    call   _printf
+    add    esp, 20
+    pop    ebp
+    ret    0
+?f@@YAXUs@@@Z ENDP    ; f
---
+++ patterns/15_structs/6_bitfields/float/float_JPN.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <memory.h>
+
+struct float_as_struct
+{
+    unsigned int fraction : 23; // 端数部分
+    unsigned int exponent : 8;  // 指数 + 0x3FF
+    unsigned int sign : 1;      // 符号ビット
+};
+
+float f(float _in)
+{
+    float f=_in;
+    struct float_as_struct t;
+
+    assert (sizeof (struct float_as_struct) == sizeof (float));
+
+    memcpy (&t, &f, sizeof (float));
+
+    t.sign=1; // 負号を設定
+    t.exponent=t.exponent+2; // dに §$2^{n}$§(nはここでは2)を乗算
+
+    memcpy (&f, &t, sizeof (float));
+
+    return f;
+};
+
+int main()
+{
+    printf ("%f\n", f(1.234));
+};
