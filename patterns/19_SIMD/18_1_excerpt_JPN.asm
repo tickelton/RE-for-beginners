b9062738a50f110080cd816273be83c8d38e707b
shmz <sysimizu@gmail.com>
2018-12-28 19:42:08 +0900

    Japanese: add 1.29 SIMD

---
+++ patterns/19_SIMD/18_1_excerpt_JPN.asm
@@ -0,0 +1,4 @@
+﻿movdqu  xmm1, xmmword ptr [ebx+edi*4] ; ar1+i*4
+movdqu  xmm0, xmmword ptr [esi+edi*4] ; ar2+i*4は16バイト境界にアラインメントされていないので、XMM0にロードされる
+paddd   xmm1, xmm0
+movdqa  xmmword ptr [eax+edi*4], xmm1 ; ar3+i*4
